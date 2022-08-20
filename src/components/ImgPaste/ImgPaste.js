370e6519fec7974990759cfb6ab3a0ccb3dba667
Kirsan23 <36918043+Kirsan23@users.noreply.github.com>
2022-08-20 17:38:29 +0300

    New Commit

--- src/components/ImgPaste/ImgPaste.js
+++ src/components/ImgPaste/ImgPaste.js
@@ -1,4 +1,10 @@
 export const ImgPaste = (props) => {
   const { sectionName, name } = props;
-  return <img className={`${sectionName}-img`} src={require(`../../img/${name}.png`)} alt="Oh..."></img>;
+  return (
+    <img
+      className={`${sectionName}-img`}
+      src={require(`../../img/${name}.png`)}
+      alt='Oh...'
+    ></img>
+  );
 };
