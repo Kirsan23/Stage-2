41d53ee594d5d9e214870756a7d62fcde1131798
Kirsan23 <36918043+Kirsan23@users.noreply.github.com>
2022-08-22 01:15:43 +0300

    New Commit

--- src/containers/HeroSection/HeroSection.js
+++ src/containers/HeroSection/HeroSection.js
@@ -3,13 +3,14 @@ import { Typography } from '../../components/Typography';
 import './HeroSection.scss';
 import { ImgPaste } from '../../components/ImgPaste/ImgPaste';
 import { SocialBlock } from '../../components/SocialBlock/SocialBlock';
+import { ForTest } from '../../components/ForTest/ForTest';
 
 export function HeroSection() {
   return (
     <section className='hero-section'>
       <div className='hero-section-main-block'>
         <div>
-          <Typography variant='h1' component='h1'>
+          {/* <Typography variant='h1' component='h1'>
             We Are The Best{' '}
           </Typography>
           <Typography variant='h1' component='h1' color='orange'>
@@ -22,9 +23,15 @@ export function HeroSection() {
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
             sint. Velit officia consequat duis enim velit mollit. Exercitation
             veniam consequat sunt nostrud amet.
-          </Typography>
+          </Typography> */}
+          <ForTest
+            heading='We Are The Best Digital Agency For Business'
+            highlight='Digital Agency'
+            paragraph='Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.'
+            component='h1'
+          ></ForTest>
         </div>
-        <Button />
+        {/* <Button /> */}
       </div>
       <ImgPaste sectionName='hero-section' name='hero-section-img'></ImgPaste>
       <SocialBlock />
