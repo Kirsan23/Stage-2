a51527bc579e3bd763ae8bc723efa286e682e3b6
Kirsan23 <36918043+Kirsan23@users.noreply.github.com>
2022-08-28 01:14:41 +0300

    New Commit

--- src/containers/ClientsFeedback/ClientsFeedback.js
+++ src/containers/ClientsFeedback/ClientsFeedback.js
@@ -7,14 +7,22 @@ import './ClientsFeedback.scss';
 export function ClientsFeedback() {
   const { darkMode } = useContext(DarkModeContext);
   return (
-    <section className={`clients-feedback ${darkMode ? `dark-theme` : `light-theme`}`}>
+    <section
+      className={`clients-feedback ${darkMode ? `dark-theme` : `light-theme`}`}
+    >
       <div className='clients-feedback-block'>
-      <InfoBlock
+        <InfoBlock
           title='Clients Feedback'
           heading={'Some Great Words From Our Clients'}
           highlight={'Great Words'}
           button='none'
         ></InfoBlock>
+        <div className='slider'>
+          <div className='slide slide-1'></div>
+          <div className='slide slide-2'></div>
+          <div className='slide slide-3'></div>
+          <button></button>
+        </div>
       </div>
     </section>
   );
