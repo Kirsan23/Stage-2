3231090461735e532c864363de9f827b837780ce
Kirsan23 <36918043+Kirsan23@users.noreply.github.com>
2022-09-04 23:42:10 +0300

    New Commit

--- src/containers/ContactUs/ContactUs.js
+++ src/containers/ContactUs/ContactUs.js
@@ -9,7 +9,7 @@ export const ContactUs = () => {
   const contacts = [
     {
       id: 1,
-      icon: 'icon-file-mail',
+      icon: 'icon-mail',
       title: 'Drop a line',
       contactsType: 'Mail Us',
       item1: 'Support87@gmial.com',
@@ -17,7 +17,7 @@ export const ContactUs = () => {
     },
     {
       id: 2,
-      icon: 'icon-file-phone-call',
+      icon: 'icon-phone-call',
       title: '24/7 Service',
       contactsType: 'Call Us',
       item1: '+880 265 98745 ',
@@ -26,7 +26,7 @@ export const ContactUs = () => {
     },
     {
       id: 3,
-      icon: 'icon-file-map-pin',
+      icon: 'icon-map-pin',
       title: 'Location',
       contactsType: 'Visit Us',
       item1: '158 ralegih sit, houston, yk 5896,uk',
@@ -45,24 +45,64 @@ export const ContactUs = () => {
           button='none'
         />
         <div className='contacts'>
-          {contacts.map(({ id, icon, title, contactsType, item1, item2, add }) => (
-            <div key={id} className='contacts-card'>
-              <div className='contacts-card-top'>
-                <span className={`${icon}`} />
-                <div>
-                  <Typography>{title}</Typography>
-                  <Typography>{contactsType}</Typography>
+          {contacts.map(
+            ({ id, icon, title, contactsType, item1, item2, add }) => (
+              <div key={id} className='contacts-cards'>
+                <div className='contacts-card-top'>
+                  <div className='card-top-icon'>
+                    <span className={`${icon}`} />
+                  </div>
+                  <div className='card-title'>
+                    <Typography
+                      className='contacts-card-title'
+                      component='h4'
+                      variant='h4'
+                      color='orange'
+                    >
+                      {title}
+                    </Typography>
+                    <Typography
+                      className='contacts-card-contactsType'
+                      component='h3'
+                      variant='h3'
+                    >
+                      {contactsType}
+                    </Typography>
+                  </div>
+                </div>
+                <div className='contacts-bottom'>
+                  <Typography
+                    className='contacts-card-item1'
+                    component='h5'
+                    variant='h5'
+                    color='gray'
+                  >
+                    {item1}
+                    {add && (
+                      <Typography
+                        className='contacts-card-add'
+
+                        variant='h5'
+                        color='orange'
+                      >
+                        {add}
+                      </Typography>
+                    )}
+                  </Typography>
+                  {item2 && (
+                    <Typography
+                      className='contacts-card-item2'
+                      component='h5'
+                      variant='h5'
+                      color='gray'
+                    >
+                      {item2}
+                    </Typography>
+                  )}
                 </div>
               </div>
-              <div className='contacts-bottom'>
-                <Typography>
-                  {item1}
-                  {add && <Typography>{add}</Typography>}
-                </Typography>
-                {item2 && <Typography>{item2}</Typography>}
-              </div>
-            </div>
-          ))}
+            )
+          )}
         </div>
       </div>
     </section>
