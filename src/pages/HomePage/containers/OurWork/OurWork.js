78c34fad093e3731e34f750b630d67c0f28e344b
Kirsan23 <beregovoi.k@gmail.com>
2022-09-13 22:16:53 +0300

    New Commit

--- src/pages/HomePage/containers/OurWork/OurWork.js
+++ src/pages/HomePage/containers/OurWork/OurWork.js
@@ -1,15 +1,13 @@
 import { InfoBlock } from '../../../../components/InfoBlock';
 import { Typography } from '../../../../components/Typography';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import './OurWork.scss';
 
 export const OurWork = () => {
   // TODO: request images for tabs from - https://github.com/unsplash/unsplash-js
 
   return (
-    <section
-      className='ourWork'
-      id='ourWork'
-    >
+    <section className='ourWork' id='ourWork'>
       <div className='sectionWrapper'>
         <InfoBlock
           title='Our Work'
@@ -17,80 +15,74 @@ export const OurWork = () => {
           highlight={'Designed & Development'}
           button='none'
         ></InfoBlock>
-        <div className='our-work-navigation'>
           {/* TODO: make tabs work */}
           {/* All / Graphic Design / UI/UX Design / Web Development - each tab needs to show different images */}
-          <Typography
-            className='our-work-navigation-unit'
-            component='h5'
-            variant='h5'
-            color='gray'
-          >
-            All
-          </Typography>
-          <Typography
-            className='our-work-navigation-unit'
-            component='h5'
-            variant='h5'
-            color='gray'
-          >
-            Graphic Design
-          </Typography>
-          <Typography
-            className='our-work-navigation-unit'
-            component='h5'
-            variant='h5'
-            color='gray'
-          >
-            UI/UX Design
-          </Typography>
-          <Typography
-            className='our-work-navigation-unit'
-            component='h5'
-            variant='h5'
-            color='gray'
-          >
-            Web Development
-          </Typography>
-        </div>
-        <div className='our-work-main-block'>
-          <div className='our-work-main-block-imgs'>
-            <img
-              className='our-work-main-block-img'
-              src={require(`../../../../img/OurWorkImg1.png`)}
-              alt='Oh...'
-            />
-            <img
-              className='our-work-main-block-img'
-              src={require(`../../../../img/OurWorkImg4.png`)}
-              alt='Oh...'
-            />
-          </div>
-          <div className='our-work-main-block-imgs'>
-            <img
-              className='our-work-main-block-img'
-              src={require(`../../../../img/OurWorkImg2.png`)}
-              alt='Oh...'
-            />
-            <img
-              className='our-work-main-block-img'
-              src={require(`../../../../img/OurWorkImg5.png`)}
-              alt='Oh...'
-            />
-          </div>
-          <div className='our-work-main-block-imgs'>
-            <img
-              className='our-work-main-block-img'
-              src={require(`../../../../img/OurWorkImg3.png`)}
-              alt='Oh...'
-            />
-            <img
-              className='our-work-main-block-img'
-              src={require(`../../../../img/OurWorkImg6.png`)}
-              alt='Oh...'
-            />
-          </div>
-        </div>
+          <Tabs className='tabWrapper'>
+            <TabList className='list'>
+              <Tab className='list-item'>
+                <Typography component='h5' variant='h5' color='gray'>
+                  All
+                </Typography>
+              </Tab>
+              <Tab className='list-item'>
+                <Typography component='h5' variant='h5' color='gray'>
+                  Graphic Design
+                </Typography>
+              </Tab>
+              <Tab className='list-item'>
+                <Typography component='h5' variant='h5' color='gray'>
+                  UI/UX Design
+                </Typography>
+              </Tab>
+              <Tab className='list-item'>
+                <Typography component='h5' variant='h5' color='gray'>
+                  Web Development
+                </Typography>
+              </Tab>
+            </TabList>
+
+            <TabPanel className='tabPanel'>
+                <div className='worksContainer'>
+                  <img
+                    className='worksContainer-img'
+                    src={require(`../../../../img/OurWorkImg1.png`)}
+                    alt='Oh...'
+                  />
+                  <img
+                    className='worksContainer-img'
+                    src={require(`../../../../img/OurWorkImg4.png`)}
+                    alt='Oh...'
+                  />
+                </div>
+                <div className='worksContainer'>
+                  <img
+                    className='worksContainer-img'
+                    src={require(`../../../../img/OurWorkImg2.png`)}
+                    alt='Oh...'
+                  />
+                  <img
+                    className='worksContainer-img'
+                    src={require(`../../../../img/OurWorkImg5.png`)}
+                    alt='Oh...'
+                  />
+                </div>
+                <div className='worksContainer'>
+                  <img
+                    className='worksContainer-img'
+                    src={require(`../../../../img/OurWorkImg3.png`)}
+                    alt='Oh...'
+                  />
+                  <img
+                    className='worksContainer-img'
+                    src={require(`../../../../img/OurWorkImg6.png`)}
+                    alt='Oh...'
+                  />
+                </div>
+            </TabPanel>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+            <TabPanel></TabPanel>
+          </Tabs>
       </div>
     </section>
   );
