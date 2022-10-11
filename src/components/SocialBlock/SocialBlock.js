ba9cae3db696f9d930124e785062dac7dba3d5a5
Kirsan23 <36918043+Kirsan23@users.noreply.github.com>
2022-10-12 00:26:33 +0300

    New Commit

--- src/components/SocialBlock/SocialBlock.js
+++ src/components/SocialBlock/SocialBlock.js
@@ -4,20 +4,20 @@ import './SocialBlock.scss';
 export const SocialBlock = (props) => {
   const { className = '', direction = 'vertical' } = props;
   const socialIcons = [
-    { id: 1, icon: 'icon-facebook-fill' },
-    { id: 2, icon: 'icon-instagram-fill' },
-    { id: 3, icon: 'icon-twitter-fill' },
-    { id: 4, icon: 'icon-skype-fill' },
+    { id: 1, icon: 'icon-facebook-fill', link: 'https://www.facebook.com/' },
+    { id: 2, icon: 'icon-instagram-fill', link: 'https://www.instagram.com/' },
+    { id: 3, icon: 'icon-twitter-fill', link: 'https://twitter.com/' },
+    { id: 4, icon: 'icon-skype-fill', link: 'https://www.skype.com/' },
   ];
 
   return (
     <div
       className={`${className ? `${className}` : 'social_block'} ${direction}`}
     >
-      {socialIcons.map(({ id, icon }) => (
-        <div key={id} className={`icon ${direction}`}>
-          <span className={`${icon}`}></span>
-        </div>
+      {socialIcons.map(({ id, icon, link }) => (
+        <a key={id} href={link} className={`icon ${direction}`}>
+          <span className={`${icon}`}/>
+        </a>
       ))}
     </div>
   );
